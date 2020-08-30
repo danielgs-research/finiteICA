c28d75a4919f660ba2831c686946c0945c0ea8e7
Mateus Marcuzzo <mateusmarcuzzo@outlook.com>
2020-08-30 19:53:39 -0300

    EDIT refactoring simulation scripts

--- simulation_scripts/sim_total_corr_pure_ICA.m
+++ simulation_scripts/sim_total_corr_pure_ICA.m
@@ -1,37 +1,40 @@
-% 04/08/2020
-% Este código é uma refatoração completa do depreciado.
-% 30/08/2020
-% este código PRETENDE ser uma refatoração do depreciado
+% NOT READY YET!!!! (30/08/2020)
+% TO-DO: refactor this
+% transform it to a script file
+
+
+% This code is greatly inspired in Painsky's code of QICA.m
+% not QICA_function.m !
+
+% 16/09/2019
+% adding these comments to prevent future problems
+% This is the pure Zipf pmf simulation.
+
 
 % Esta linha indica que é um script-file
 1;
 
-function bss_and_total_corr_simulation(some_primes,n_sources,n_samples,n_trials,KLD_thresh)
+%% By now it englobes just the pure_zipf experiment
+function total_corr_pure_ICA_simulation(some_primes,n_sources,n_samples,
+										n_trials,qica_min_k,qica_max_k,the_distribution)
 	sim_start_time = localtime(time());
 
 	% Must be a cel array, so we can do a strfind...
-	algorithms_names = {'america';'sa4ica';'GLICA'}
+	algorithms_names = {'america';'sa4ica';'QICA';'GLICA'}
 
 	the_algorithms = 1:length(algorithms_names);
 
+
 	space = [ length(some_primes), length(n_sources), length(n_samples),...
-	 length(the_algorithms) n_trials ];
+	 length(the_algorithms) ];
 
 	n_cases = prod(space);
 
-	bss_succ_rate = logical(zeros( [ space ] ));
+	trial_time = zeros( [ size(zeros( [ space ] )), n_trials] );
 
-	bss_trial_time = zeros( [ size(bss_succ_rate) ] );
-
-	total_corr_results = zeros( [size(bss_trial_time) ] );
-
-	% this threshold is such that
-	% there is a gap of "threshold" in the kullback-leibler divergence
-	% it is because uniform distributions are unsolvable.
-	threshold = KLD_thresh;
+	total_corr_results = zeros( [size(trial_time) ] );
 
 	total_time = tic;
-
 
 	for p_i = 1:length(some_primes)
 		for k_i = 1:length(n_sources)
@@ -45,7 +48,7 @@ function bss_and_total_corr_simulation(some_primes,n_sources,n_samples,n_trials,
 				Nobs = n_samples(t_i)
 	     		fprintf('\n');
 
-	            tic;
+	            
 				for trial=1:n_trials
 
 					PK = P^K;
@@ -68,6 +71,33 @@ function bss_and_total_corr_simulation(some_primes,n_sources,n_samples,n_trials,
 
 					%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+	                %First we are going Zipf like Painsky's code
+	                q=P;
+	                n=K; %dimension of the random vector 
+	                %Zipf distribution
+	                s=1.05; %zipf distribution parameter
+	                zipf_p=[1:1:q^n];
+	                zipf_p=1./(zipf_p.^s);
+	                zipf_p=zipf_p'/sum(zipf_p);
+
+	                joint_pmf  = zipf_p';
+	                
+	                generated_integers = gerainteiros_from_probs(joint_pmf,P,K,Nobs);
+	                X = mapeiainteiro_to_tuple(generated_integers,P,K);
+	                X = X';
+	                
+	                
+	                Y = mapeiapermutacao([],X,1:PK,P,K);
+	                % A permutacao trivial em X deve dar o pr�prio X
+	                % Comentar este c�digo quando seguro de consist�ncia
+	                assert( all( all( X==Y ) ) );
+	                
+	                % THe -1 is necessary,since the first element is always zero.
+	                estimated_joint_from_samples = estimate_marg_probs(generated_integers'-1,PK);
+	                
+	                h_joint_from_samples = entropy_from_frequencies(estimated_joint_from_samples');
+	                
+	%                disp('gerou joint pmf');
 
 					parameters.P = P;
 					parameters.K = K;
@@ -75,15 +105,6 @@ function bss_and_total_corr_simulation(some_primes,n_sources,n_samples,n_trials,
 					parameters.Lex = Lex;
 					parameters.r = r;
 
-					% from Daniel code
-					% gives the pmfs of each source and generate the Samples
-					[the_pmfs, S] = geravetorsinais(P,1,K,threshold,Nobs);
-
-					% generates the mixing_matrix/mix_matrix
-					A = geramatrizmistura(P,1,[],K);
-
-					% the miXed Samples
-					X = produtomatrizGF(A,S,P,1,[]);
 	               
 
 			        % calculo tensor de probabilidades - AMERICA
@@ -96,6 +117,13 @@ function bss_and_total_corr_simulation(some_primes,n_sources,n_samples,n_trials,
 			        Px=Px/Nobs;
 			        
 			        h_joint=-sum(Px(Px>0).*log2(Px(Px>0)));
+	                
+	                % a toler�ncia aqui � de 0.2
+	                assert( h_joint <= h_joint_from_samples + 0.2);
+	                assert( h_joint >= h_joint_from_samples - 0.2);
+	%                disp('h_joint - h_joint_from_samples');
+	%                disp(h_joint - h_joint_from_samples);
+	                
 
 		        	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 			        %%%%% AMERICA ALGORITHM EVALUATION %%%%
@@ -105,25 +133,25 @@ function bss_and_total_corr_simulation(some_primes,n_sources,n_samples,n_trials,
 	                
 			        start_time = tic;
 			        [Wm] = america(Px,parameters);
-			        bss_trial_time(p_i,k_i,t_i,algo_i,trial) = toc(start_time);
+			        trial_time(p_i,k_i,t_i,algo_i,trial) = toc(start_time);
 			        
 			        % This is the demixing matrix, we are going to do U*A
 			        % and check the number of hits. If everyone hits, we count it
-			        U = produtomatrizGF(Wm,A,P,1,[]);
+	%		        U = produtomatrizGF(Wm,A,P,1,[]);
 	%                U_america = U
 
 			        % there's some legacy code happening here, we'll leave it here. 
 			        % it resembles supercomparativo.m code
-			        if(1>1)
-			            Z = (U>-1); %null element in GF(P^1) 
-			        else
-			            Z = (U>0); 
-			        end
+	%		        if(1>1)
+	%		            Z = (U>-1); %null element in GF(P^1) 
+	%		        else
+	%		            Z = (U>0); 
+	%		        end
 
-			        hits = sum(sum(Z,2)==1);    
-			        if(hits == K)  
-			        	bss_succ_rate(p_i,k_i,t_i,algo_i,trial) += 1;
-			        end 
+	%		        hits = sum(sum(Z,2)==1);    
+	%		        if(hits == K)  
+	%		        	bss_succ_rate(p_i,k_i,t_i,algo_i) += 1;
+	%		        end 
 
 			        Y = produtomatrizGF(Wm,X,P,1,[]);
 	%                Y_possible_tuples_america = produtomatrizGF(U_america,all_possible_tuples,P,1,[])
@@ -146,22 +174,22 @@ function bss_and_total_corr_simulation(some_primes,n_sources,n_samples,n_trials,
 	                % the decode_ function, makes a confusion with a pre-existing
 	                % decode function. so I renamed it to decode_. It is part of sa4ica
 			        [Wsa] = sa4ica_decode(Px,parameters,0.995,5);
-					bss_trial_time(p_i,k_i,t_i,algo_i,trial) = toc(start_time);
+					trial_time(p_i,k_i,t_i,algo_i,trial) = toc(start_time);
 
 					%once again, we are going to test the number of hits
-			        U = produtomatrizGF(Wsa,A,P,1,[]);
+	%		        U = produtomatrizGF(Wsa,A,P,1,[]);
 	%                Usa4ica = U
 	                
-			        if(1>1)
-			            Z = (U>-1); %null element in GF(q^m) 
-			        else
-			            Z = (U>0); 
-			        end
+	%		        if(1>1)
+	%		            Z = (U>-1); %null element in GF(q^m) 
+	%		        else
+	%		            Z = (U>0); 
+	%		        end
 
-			        hits = sum(sum(Z,2)==1);    
-			        if(hits == K)  
-			        	bss_succ_rate(p_i,k_i,t_i,algo_i,trial) += 1;
-			        end 
+	%		        hits = sum(sum(Z,2)==1);    
+	%		        if(hits == K)  
+	%		        	bss_succ_rate(p_i,k_i,t_i,algo_i) += 1;
+	%		        end 
 
 	                Y = produtomatrizGF(Wsa,X,P,1,[]);
 	%                Y_possible_tuples_sa4ica = produtomatrizGF(Usa4ica,all_possible_tuples,P,1,[])
@@ -175,31 +203,35 @@ function bss_and_total_corr_simulation(some_primes,n_sources,n_samples,n_trials,
 			        %%%%%        WHICH IS PURE ICA     %%%%
 			        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 	                
-	%                 start_time = tic;
+	                algo_i = find(strcmp(algorithms_names,'QICA'));
+
+	                 start_time = tic;
 	                
-	%                  % this is the gradient descent approach. the exhaustive may need too much time
-	% 		       	[opt_est_vals,opt_appox_lin_min2,opt_appox_ent_with_appox_vals2,opt_perm,opt_v_vec]=QICA_function(K,P,Px',1,1,10,1000);
+
+	 		       	[opt_est_vals,opt_appox_lin_min2,opt_appox_ent_with_appox_vals2,opt_perm,opt_v_vec]=QICA_function(K,P,Px',0,qica_min_k,qica_max_k,1000);
 	% %                opt_perm
-	% %		        [opt_p,opt_perm,est_vals,marginals_after]=BICA_function(Px',4,10);                          
+	 %		        [opt_p,opt_perm,est_vals,marginals_after]=BICA_function(Px',4,10);                          
 	% %                opt_perm'
 	            
 
-	% 		        bss_trial_time(p_i,k_i,t_i,3,trial) = toc(start_time);
+	 		        trial_time(p_i,k_i,t_i,algo_i,trial) = toc(start_time);
+	                %The below line was meant for testing.
+	%                toc(start_time);
 
-	% 		        [Yqica,maxhit,Y_possible_tuples_qica] = mapeiapermutacao(S, X, opt_perm,P,K);
+	 		        [Yqica,] = mapeiapermutacao([], X, opt_perm,P,K);
 	%                 Y_possible_tuples_qica
 	                
 	% 		        if(maxhit == K)  
-	% 		        	bss_succ_rate(p_i,k_i,t_i,3) += 1;
+	% 		        	bss_succ_rate(p_i,k_i,t_i,algo_i) += 1;
 	% 		        end
 	                
 	                
 	          %       fprintf('maxhit is %d where K = %d\n',maxhit,K);
 			          
-			        % h_marg=entropy_from_frequencies(estimate_marg_probs(Yqica,P)');
-			        % h_marg = sum(h_marg(:));
+			         h_marg=entropy_from_frequencies(estimate_marg_probs(Yqica,P)');
+			         h_marg = sum(h_marg(:));
 
-			        % total_corr_results(p_i,k_i,t_i,3,trial) = h_marg-h_joint;
+			         total_corr_results(p_i,k_i,t_i,algo_i,trial) = h_marg-h_joint;
 	            	        	
 	                %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 			        %%%%%  GLICA ALGORITHM EVALUATION  %%%%
@@ -209,22 +241,22 @@ function bss_and_total_corr_simulation(some_primes,n_sources,n_samples,n_trials,
 	            
 	                start_time = tic;
 			        [Wglica] = GLICA_function(X,parameters);
-			        bss_trial_time(p_i,k_i,t_i,algo_i,trial) = toc(start_time);
+			        trial_time(p_i,k_i,t_i,algo_i,trial) = toc(start_time);
 	                
 	                %once again, we are going to test the number of hits
-			        U = produtomatrizGF(Wglica,A,P,1,[]);
+	%		        U = produtomatrizGF(Wglica,A,P,1,[]);
 	%                Uglica = U
 
-			        if(1>1)
-			            Z = (U>-1); %null element in GF(q^m) 
-			        else
-			            Z = (U>0); 
-			        end
+	%		        if(1>1)
+	%		            Z = (U>-1); %null element in GF(q^m) 
+	%		        else
+	%		            Z = (U>0); 
+	%		        end
 
-			        hits = sum(sum(Z,2)==1);    
-			        if(hits == K)  
-			        	bss_succ_rate(p_i,k_i,t_i,algo_i,trial) += 1;
-			        end 
+	%		        hits = sum(sum(Z,2)==1);    
+	%		        if(hits == K)  
+	%		        	bss_succ_rate(p_i,k_i,t_i,algo_i) += 1;
+	%		        end 
 	                
 	                
 	                
@@ -235,29 +267,23 @@ function bss_and_total_corr_simulation(some_primes,n_sources,n_samples,n_trials,
 			        total_corr_results(p_i,k_i,t_i,algo_i,trial) = h_marg-h_joint;
 	                
 	                
-			  	end 
-		  		toc;
+			  	end
 			end
 		end
 	end
 	toc(total_time)
 
-	mean_bss_succ_rate = mean(bss_succ_rate,5);
-	mean_bss_trial_time = mean(bss_trial_time, 5);
+
+	mean_trial_time = mean(trial_time, 5);
 	mean_total_corr_results = mean(total_corr_results, 5);
+
 
 	% saves with the date (day/month/year) and the hour: hh:mm 
 	% start and ending times
 	start_time_str = strftime("sim_data_start_%d_%m_%Y_%H_%M",sim_start_time);
-
-	
-	saved_sim_str = strftime("_end_%d_%m_%Y_%H_%M_sim_BSS_and_total_corr",localtime(time()));
-
+	saved_sim_str = strftime("_end_%d_%m_%Y_%H_%M_sim_total_corr_pure_ICA",localtime(time()));
 
 	saved_sim = sprintf("sim_data/%s%s",start_time_str,saved_sim_str);
 	save(saved_sim)
+
 end
-
-
-
-
